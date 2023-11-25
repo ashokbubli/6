cfaa75edd6c7e4e66d4ff162b94493d595c3d204
ashokbubli <90835938+ashokbubli@users.noreply.github.com>
2023-11-25 18:59:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 fetch("repository_metadata.json")
-	.then(function(response){
-		return response.json();
-	})
-	.then(function(metadata){
-		let placeholder = document.querySelector("#data-output");
-		let out = "";
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (metadata) {
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
 
-		for(let item of metadata){
-			out += `
-				<tr>
-					<td>${item.Repository}</td>
-					<td>${item.Application}</td>
-					<td>${item["IT Owner"]}</td>
-					<td>${item["Key Expert"]}</td>
-					<td>${item["Hosted Environment"]}</td>
-					<td>${item.Accessibility}</td>
-					<td>${item["Business Service Name"]}</td>
-				</tr>
-			`;
-		}
+        for (let repoName in metadata) {
+            let repoData = metadata[repoName];
+            let keyExpert = repoData.Contacts["Key-Expert"].join(", ");
 
-		placeholder.innerHTML = out;
-	});
+            out += `
+                <tr>
+                    <td>${repoName}</td>
+                    <td>${repoData.Application}</td>
+                    <td>${repoData.Contacts["IT Owner"]}</td>
+                    <td>${keyExpert}</td>
+                    <td>${repoData.Contacts["Hosted Environment"]}</td>
+                    <td>${repoData.Contacts.Accessibility}</td>
+                    <td>${repoData.BMC["Business Service Name"]}</td>
+                </tr>
+            `;
+        }
+
+        placeholder.innerHTML = out;
+    });
