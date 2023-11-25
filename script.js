157c5b0212b5985a200c0e3df049ee58ca818e9a
ashokbubli <90835938+ashokbubli@users.noreply.github.com>
2023-11-25 20:55:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ fetch("repository_metadata.json")
                 out += `
                     <tr>
                         <td>${repoName}</td>
-                        <td>${repoData.Application}</td>
-                        <td>${repoData.Contacts["IT Owner"]}</td>
-                        <td>${repoData.Contacts["Key-Expert"].join(', ')}</td>
-                        <td>${repoData.Contacts["Hosted Environment"]}</td>
-                        <td>${repoData.Contacts.Accessibility}</td>
-                        <td>${repoData.BMC["Business Service Name"]}</td>
+                        <td>${repoData.application}</td>
+                        <td>${repoData.contacts["it-owner"]}</td>
+                        <td>${repoData.contacts["key-expert"].join(', ')}</td>
+                        <td>${repoData.contacts["hosted-env"]}</td>
+                        <td>${repoData.contacts.accessibility}</td>
+                        <td>${repoData.supports["business-service-name"]}</td>
                     </tr>
                 `;
             }
