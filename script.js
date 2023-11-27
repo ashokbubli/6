0a3b0646545ccb979d24b76cd52fa7374867eb8d
ashokbubli <90835938+ashokbubli@users.noreply.github.com>
2023-11-27 18:51:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,37 +12,37 @@ fetch("repository_metadata.json")
             out += "<tr>";
             out += `<td>${key}</td>`;
             
-            if (item.application !== undefined && item.application !== null) {
+            if (item.application !== undefined && item.application !== Null) {
                 out += `<td>${item.application}</td>`;
             } else {
                 out += "<td></td>";
             }
 
-            if (item.contacts !== undefined && item.contacts["it-owner"] !== undefined && item.contacts["it-owner"] !== null) {
+            if (item.contacts !== undefined && item.contacts["it-owner"] !== undefined && item.contacts["it-owner"] !== Null) {
                 out += `<td>${item.contacts["it-owner"]}</td>`;
             } else {
                 out += "<td></td>";
             }
 
-            if (item.contacts !== undefined && item.contacts["key-expert"] !== undefined && item.contacts["key-expert"] !== null) {
+            if (item.contacts !== undefined && item.contacts["key-expert"] !== undefined && item.contacts["key-expert"] !== Null) {
                 out += `<td>${item.contacts["key-expert"].join(', ')}</td>`;
             } else {
                 out += "<td></td>";
             }
 
-            if (item.contacts !== undefined && item.contacts["hosted-env"] !== undefined && item.contacts["hosted-env"] !== null) {
+            if (item.contacts !== undefined && item.contacts["hosted-env"] !== undefined && item.contacts["hosted-env"] !== Null) {
                 out += `<td>${item.contacts["hosted-env"]}</td>`;
             } else {
                 out += "<td></td>";
             }
 
-            if (item.contacts !== undefined && item.contacts.accessibility !== undefined && item.contacts.accessibility !== null) {
+            if (item.contacts !== undefined && item.contacts.accessibility !== undefined && item.contacts.accessibility !== Null) {
                 out += `<td>${item.contacts.accessibility}</td>`;
             } else {
                 out += "<td></td>";
             }
 
-            if (item.servicenow !== undefined && item.servicenow["business-service-name"] !== undefined && item.servicenow["business-service-name"] !== null) {
+            if (item.servicenow !== undefined && item.servicenow["business-service-name"] !== undefined && item.servicenow["business-service-name"] !== Null) {
                 out += `<td>${item.servicenow["business-service-name"]}</td>`;
             } else {
                 out += "<td></td>";
