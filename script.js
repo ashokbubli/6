8c83159c259a9abe4df971662a58c907ab37bb9e
ashokbubli <90835938+ashokbubli@users.noreply.github.com>
2023-11-27 20:13:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,30 +53,54 @@ fetch("repository_metadata.json")
 
         placeholder.innerHTML = out;
 
-        // Add event listeners for filter inputs
-        let filterInputs = document.querySelectorAll(".filter-input");
-        filterInputs.forEach(function(input) {
-            input.addEventListener("input", function() {
-                filterTable(input);
+        // Add event listeners for filtering
+        const filterRepository = document.getElementById('filterRepository');
+        const filterApplication = document.getElementById('filterApplication');
+        const filterITOwner = document.getElementById('filterITOwner');
+        const filterKeyExpert = document.getElementById('filterKeyExpert');
+        const filterHostedEnv = document.getElementById('filterHostedEnv');
+        const filterAccessibility = document.getElementById('filterAccessibility');
+        const filterBusinessService = document.getElementById('filterBusinessService');
+
+        const rows = document.querySelectorAll('#data-output tr');
+
+        function filterTable() {
+            const repositoryValue = filterRepository.value.toLowerCase();
+            const applicationValue = filterApplication.value.toLowerCase();
+            const itOwnerValue = filterITOwner.value.toLowerCase();
+            const keyExpertValue = filterKeyExpert.value.toLowerCase();
+            const hostedEnvValue = filterHostedEnv.value.toLowerCase();
+            const accessibilityValue = filterAccessibility.value.toLowerCase();
+            const businessServiceValue = filterBusinessService.value.toLowerCase();
+
+            rows.forEach(row => {
+                const repository = row.children[0].textContent.toLowerCase();
+                const application = row.children[1].textContent.toLowerCase();
+                const itOwner = row.children[2].textContent.toLowerCase();
+                const keyExpert = row.children[3].textContent.toLowerCase();
+                const hostedEnv = row.children[4].textContent.toLowerCase();
+                const accessibility = row.children[5].textContent.toLowerCase();
+                const businessService = row.children[6].textContent.toLowerCase();
+
+                const showRow = (
+                    repository.includes(repositoryValue) &&
+                    application.includes(applicationValue) &&
+                    itOwner.includes(itOwnerValue) &&
+                    keyExpert.includes(keyExpertValue) &&
+                    hostedEnv.includes(hostedEnvValue) &&
+                    accessibility.includes(accessibilityValue) &&
+                    businessService.includes(businessServiceValue)
+                );
+
+                row.style.display = showRow ? '' : 'none';
             });
-        });
-    });
-
-function filterTable(input) {
-    let columnIndex = input.dataset.column;
-    let filterValue = input.value.toUpperCase();
-    let table = document.querySelector("table");
-    let rows = table.querySelectorAll("tbody tr");
-
-    rows.forEach(function(row) {
-        let cells = row.getElementsByTagName("td")[columnIndex];
-        if (cells) {
-            let cellText = cells.textContent || cells.innerText;
-            if (cellText.toUpperCase().indexOf(filterValue) > -1) {
-                row.style.display = "";
-            } else {
-                row.style.display = "none";
-            }
         }
+
+        filterRepository.addEventListener('input', filterTable);
+        filterApplication.addEventListener('input', filterTable);
+        filterITOwner.addEventListener('input', filterTable);
+        filterKeyExpert.addEventListener('input', filterTable);
+        filterHostedEnv.addEventListener('input', filterTable);
+        filterAccessibility.addEventListener('input', filterTable);
+        filterBusinessService.addEventListener('input', filterTable);
     });
-}
