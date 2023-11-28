ca03c1df443cf051284e13a6e1eb51644ba7ed98
ashokbubli <90835938+ashokbubli@users.noreply.github.com>
2023-11-28 11:54:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
-function applyFilter() {
-    let filterRepositoryValue = filterRepositoryInput.value.toLowerCase();
-    let filterApplicationValue = filterApplicationInput.value.toLowerCase();
+fetch("repository_metadata.json")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(metadata){
+        let placeholder = document.querySelector("#data-output");
+        let filterRepositoryInput = document.querySelector("#filter-repository");
+        let filterApplicationInput = document.querySelector("#filter-application");
+        let filterRow = document.getElementById("filter-row");
+        let out = "";
 
-    let filteredMetadata = Object.keys(metadata).filter(key => {
-        let item = metadata[key];
-        let applicationValue = (item.application || "").toLowerCase();
+        function applyFilter() {
+            let filterRepositoryValue = filterRepositoryInput.value.toLowerCase();
+            let filterApplicationValue = filterApplicationInput.value.toLowerCase();
 
-        // Include rows where the "Application" column is empty or null
-        let hasEmptyApplication = filterApplicationValue === "" && (applicationValue === "" || applicationValue === "null");
+            let filteredMetadata = Object.keys(metadata).filter(key =>
+                key.toLowerCase().includes(filterRepositoryValue) &&
+                (metadata[key].application || "").toLowerCase().includes(filterApplicationValue)
+            );
 
-        // Filter based on repository and application values
-        return key.toLowerCase().includes(filterRepositoryValue) &&
-            (applicationValue.includes(filterApplicationValue) || hasEmptyApplication);
+            out = "";
+            filteredMetadata.forEach(key => {
+                let item = metadata[key];
+
+                out += "<tr>";
+                out += `<td>${key}</td>`;
+                out += `<td>${item.application || ""}</td>`;
+                out += `<td>${item.contacts ? item.contacts["it-owner"] || "" : ""}</td>`;
+                out += `<td>${item.contacts ? (item.contacts["key-expert"] ? item.contacts["key-expert"].join(', ') : "") : ""}</td>`;
+                out += `<td>${item.contacts ? item.contacts["hosted-env"] || "" : ""}</td>`;
+                out += `<td>${item.contacts ? item.contacts.accessibility || "" : ""}</td>`;
+                out += `<td>${item.servicenow ? item.servicenow["business-service-name"] || "" : ""}</td>`;
+                out += "</tr>";
+            });
+
+            placeholder.innerHTML = out;
+        }
+
+        // Initial rendering
+        applyFilter();
+
+        // Add event listeners for real-time filtering
+        filterRepositoryInput.addEventListener("input", applyFilter);
+        filterApplicationInput.addEventListener("input", applyFilter);
+
+        // Function to toggle filter row visibility
+        window.toggleFilter = function (filterId) {
+            let filterRepositoryInput = document.querySelector("#filter-repository");
+            let filterApplicationInput = document.querySelector("#filter-application");
+
+            filterRepositoryInput.value = ''; // Clear repository filter
+            filterApplicationInput.value = ''; // Clear application filter
+
+            if (filterId === 'filter-repository') {
+                filterApplicationInput.dispatchEvent(new Event('input')); // Trigger application filter
+            } else {
+                filterRepositoryInput.dispatchEvent(new Event('input')); // Trigger repository filter
+            }
+
+            let filterInput = document.getElementById(filterId);
+            filterRow.style.display = (filterRow.style.display === "none") ? "table-row" : "none";
+            filterInput.focus();
+        };
     });
-
-    out = "";
-    filteredMetadata.forEach(key => {
-        let item = metadata[key];
-
-        out += "<tr>";
-        out += `<td>${key}</td>`;
-        out += `<td>${item.application || ""}</td>`;
-        out += `<td>${item.contacts ? item.contacts["it-owner"] || "" : ""}</td>`;
-        out += `<td>${item.contacts ? (item.contacts["key-expert"] ? item.contacts["key-expert"].join(', ') : "") : ""}</td>`;
-        out += `<td>${item.contacts ? item.contacts["hosted-env"] || "" : ""}</td>`;
-        out += `<td>${item.contacts ? item.contacts.accessibility || "" : ""}</td>`;
-        out += `<td>${item.servicenow ? item.servicenow["business-service-name"] || "" : ""}</td>`;
-        out += "</tr>";
-    });
-
-    placeholder.innerHTML = out;
-}
