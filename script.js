9f30f9244f0b148a8392986b6219c1b90e7da755
ashokbubli <90835938+ashokbubli@users.noreply.github.com>
2023-12-02 19:13:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-function applyFilter() {
-    let filterRepositoryValue = filterRepositoryInput.value.toLowerCase();
-    let filterApplicationValue = filterApplicationInput.value.toLowerCase();
+fetch("repository_metadata.json")
+    .then(function (response) {
+// Extract the Last modified date and time header from the response 
+        const lastModified = new Date(response.headers.get('Last-Modified'));
 
-    let filteredMetadata = Object.keys(metadata).filter(key =>
-        key.toLowerCase().includes(filterRepositoryValue) &&
-        (metadata[key].application !== undefined && metadata[key].application !== null && metadata[key].application.toLowerCase().includes(filterApplicationValue))
-    );
+        return response.json()
+            .then(function (metadata) {
+                let placeholder = document.querySelector("#data-output"); 
+                let filterRepositoryInput = document.querySelector("#filter-repository");
+                let filterApplicationInput = document.querySelector("#filter-application");
+                let filterRow = document.getElementById("filter-row");
+                let infoContainer = document.getElementById("info-container");
+                let totalRepositoriesElement = document.getElementById("total-repositories");
+                let lastUpdatedElement = document.getElementById("last-updated");
 
-    out = "";
-    filteredMetadata.forEach(key => {
-        let item = metadata[key];
+                let out = "";
 
-        out += "<tr>";
-        out += `<td>${key}</td>`;
-        out += `<td>${item.application || "N/A"}</td>`;
-        out += `<td>${item.contacts ? item.contacts["it-owner"] || "" : ""}</td>`;
-        out += `<td>${item.contacts ? (item.contacts["key-expert"] ? item.contacts["key-expert"].join(', ') : "") : ""}</td>`;
-        out += `<td>${item.contacts ? item.contacts["hosted-env"] || "" : ""}</td>`;
-        out += `<td>${item.contacts ? item.contacts.accessibility || "" : ""}</td>`;
-        out += `<td>${item.servicenow ? item.servicenow["business-service-name"] || "" : ""}</td>`;
-        out += "</tr>";
+                function applyFilter() {
+                    let filterRepositoryValue = filterRepositoryInput.value.toLowerCase(); 
+                    let filterApplicationValue = filterApplicationInput.value.toLowerCase();
+
+                    let filteredMetadata = Object.keys(metadata).filter(key =>
+                        key.toLowerCase().includes(filterRepositoryValue) &&
+                        (metadata[key].application === null || metadata[key].application === undefined || metadata[key].application === '' || metadata[key].application.toLowerCase().includes(filterApplicationValue))
+                    );
+
+                    out = "";
+                    filteredMetadata.forEach(key => {
+                        let item = metadata[key];
+
+                        out += "<tr>";
+                        out += `<td>${key}</td>`;
+                        out += `<td>${item.application || ""}</td>`;
+                        out += `<td>${item.contacts ? item.contacts["it-owner"] || "" : ""}</td>`;
+                        out += `<td>${item.contacts ? (item.contacts["key-expert"] ? item.contacts["key-expert"].join(', ') : "") : ""}</td>`;
+                        out += `<td>${item.contacts ? item.contacts["hosted-env"] || "" : ""}</td>`; 
+                        out += `<td>${item.contacts ? item.contacts.accessibility || "" : ""}</td>`;
+                        out += `<td>${item.servicenow ? item.servicenow["business-service-name"] || "" : ""}</td>`;
+                        out += "</tr>"; 
+                    });
+
+                    placeholder.innerHTML = out;
+                    totalRepositoriesElement.innerHTML = `Total number of Repositories = ${filteredMetadata.length}`;
+
+                    // Display the Last updated Info
+                    lastUpdatedElement.innerHTML = `Last updated: ${lastModified.toLocaleString()}`;
+                }
+
+                // Initial rendering
+                applyFilter();
+
+                // Add event listeners for real time filtering
+                filterRepositoryInput.addEventListener("input", applyFilter);
+                filterApplicationInput.addEventListener("input", applyFilter);
+
+                // Function to toggle filter row visibility 
+                window.toggleFilter = function (filterId) {
+                    let filterInput = document.getElementById(filterId);
+                    filterRow.style.display = (filterRow.style.display === "none") ? "table-row" : "none";
+                    filterInput.focus();
+                };
+            });
     });
-
-    placeholder.innerHTML = out;
-    totalRepositoriesElement.innerHTML = `Total number of Repositories = ${filteredMetadata.length}`;
-
-    // Display the Last updated Info
-    lastUpdatedElement.innerHTML = `Last updated: ${lastModified.toLocaleString()}`;
-}
