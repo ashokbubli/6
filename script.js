4c825f20dd65dcf0a4f08d636a0cf100ffbc5cc2
ashokbubli <90835938+ashokbubli@users.noreply.github.com>
2023-12-06 21:51:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,77 @@
-fetch("repository_metadata.json")
-    .then(function (response) {
-        // Extract the Last-Modified header from the response
-        const lastModified = new Date(response.headers.get('Last-Modified'));
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to handle the data and update the UI
+    function updateUI(metadata, lastModified) {
+        let placeholder = document.querySelector("#data-output");
+        let filterRepositoryInput = document.querySelector("#filter-repository");
+        let filterApplicationInput = document.querySelector("#filter-application");
+        let filterRow = document.getElementById("filter-row");
+        let totalRepositoriesElement = document.getElementById("total-repositories");
+        let lastUpdatedElement = document.getElementById("last-updated");
 
-        return response.json()
-            .then(function (metadata) {
-                let placeholder = document.querySelector("#data-output");
-                let filterRepositoryInput = document.querySelector("#filter-repository");
-                let filterApplicationInput = document.querySelector("#filter-application");
-                let filterRow = document.getElementById("filter-row");
-                let infoContainer = document.getElementById("info-container");
-                let totalRepositoriesElement = document.getElementById("total-repositories");
-                let lastUpdatedElement = document.getElementById("last-updated");
+        function applyFilter() {
+            let filterRepositoryValue = filterRepositoryInput.value.toLowerCase();
+            let filterApplicationValue = filterApplicationInput.value.toLowerCase();
 
-                let out = "";
+            let filteredMetadata = Object.keys(metadata).filter(key => {
+                let item = metadata[key];
+                let repositoryValue = key.toLowerCase();
+                let applicationValue = item.application !== null ? item.application.toLowerCase() : "";
 
-                function applyFilter() {
-                    let filterRepositoryValue = filterRepositoryInput.value.toLowerCase();
-                    let filterApplicationValue = filterApplicationInput.value.toLowerCase();
-
-                    let filteredMetadata = Object.keys(metadata).filter(key =>
-                        key.toLowerCase().includes(filterRepositoryValue) &&
-                        (metadata[key].application === null || metadata[key].application === undefined || metadata[key].application === '' || metadata[key].application.toLowerCase().includes(filterApplicationValue))
-                    );
-
-                    out = "";
-                    filteredMetadata.forEach(key => {
-                        let item = metadata[key];
-
-                        out += "<tr>";
-                        out += `<td>${key}</td>`;
-                        out += `<td>${item.application || ""}</td>`;
-                        out += `<td>${item.contacts ? item.contacts["it-owner"] || "" : ""}</td>`;
-                        out += `<td>${item.contacts ? (item.contacts["key-expert"] ? item.contacts["key-expert"].join(', ') : "") : ""}</td>`;
-                        out += `<td>${item.contacts ? item.contacts["hosted-env"] || "" : ""}</td>`;
-                        out += `<td>${item.contacts ? item.contacts.accessibility || "" : ""}</td>`;
-                        out += `<td>${item.servicenow ? item.servicenow["business-service-name"] || "" : ""}</td>`;
-                        out += "</tr>";
-                    });
-
-                    placeholder.innerHTML = out;
-                    totalRepositoriesElement.innerHTML = `Total number of Repositories = ${filteredMetadata.length}`;
-
-                    // Display Last updated information
-                    lastUpdatedElement.innerHTML = `Last updated: ${lastModified.toLocaleString()}`;
-                }
-
-                // Initial rendering
-                applyFilter();
-
-                // Add event listeners for real-time filtering
-                filterRepositoryInput.addEventListener("input", applyFilter);
-                filterApplicationInput.addEventListener("input", applyFilter);
-
-                // Function to toggle filter row visibility
-                window.toggleFilter = function (filterId) {
-                    let filterInput = document.getElementById(filterId);
-                    filterRow.style.display = (filterRow.style.display === "none") ? "table-row" : "none";
-                    filterInput.focus();
-                };
+                return repositoryValue.includes(filterRepositoryValue) &&
+                    (applicationValue.includes(filterApplicationValue) ||
+                        (filterApplicationValue === "blank" && (applicationValue === "null" || applicationValue === "")));
             });
-    });
+
+            let out = "";
+            filteredMetadata.forEach(key => {
+                let item = metadata[key];
+
+                out += "<tr>";
+                out += `<td>${key}</td>`;
+                out += `<td>${item.application !== null ? item.application : ""}</td>`;
+                out += `<td>${item.contacts ? item.contacts["it-owner"] || "" : ""}</td>`;
+                out += `<td>${item.contacts ? (item.contacts["key-expert"] ? item.contacts["key-expert"].join(', ') : "") : ""}</td>`;
+                out += `<td>${item.contacts ? item.contacts["hosted-env"] || "" : ""}</td>`;
+                out += `<td>${item.contacts ? item.contacts.accessibility || "" : ""}</td>`;
+                out += `<td>${item.servicenow ? item.servicenow["business-service-name"] || "" : ""}</td>`;
+                out += "</tr>";
+            });
+
+            placeholder.innerHTML = out;
+            totalRepositoriesElement.innerHTML = `Total number of Repositories = ${filteredMetadata.length}`;
+
+            // Display the Last updated Info
+            lastUpdatedElement.innerHTML = `Last updated: ${lastModified.toLocaleString()}`;
+        }
+
+        // Initial rendering
+        applyFilter();
+
+        // Add event listeners for real-time filtering
+        filterRepositoryInput.addEventListener("input", applyFilter);
+        filterApplicationInput.addEventListener("input", applyFilter);
+
+        // Function to toggle filter row visibility
+        window.toggleFilter = function (filterId) {
+            let filterInput = document.getElementById(filterId);
+            filterRow.style.display = (filterRow.style.display === "none") ? "table-row" : "none";
+            filterInput.focus();
+        };
+    }
+
+    // Fetch data and handle errors
+    fetch("repository_metadata.json")
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(function (metadata) {
+            const lastModified = new Date(response.headers.get('Last-Modified'));
+            updateUI(metadata, lastModified);
+        })
+        .catch(function (error) {
+            console.error('Error fetching data:', error);
+        });
+});
